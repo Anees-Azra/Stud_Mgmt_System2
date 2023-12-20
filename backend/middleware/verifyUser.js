e4d765968d2a00c2038e76e55f7d26eaa5126ba9
Anees Azra <aneesazra002@gmail.com>
2023-12-20 11:40:17 -0500

    comitted on 12/20/2023

--- backend/middleware/verifyUser.js
+++ backend/middleware/verifyUser.js
@@ -1,89 +1,89 @@
-import jwt from 'jsonwebtoken';
-
-const secretKey = "8325731cde8293dbe25e329450ae2aa60486002f6eba23faacb69a9a18952e43";
-
-const verifyUser = (req, res, next) => {
-
-    console.log("req.headers" , req.headers);
-    const bearerHeader = req.headers['authorization'];
-    console.log("bearerHeader" , bearerHeader, )
-
-    // if (bearerHeader) {
-    //     const token = bearerHeader.split(' ')[1];
-    //     // Rest of your code using 'token'
-    // } else {
-    //     // Handle the case when 'bearerHeader' is undefined
-    //     console.error('Bearer header is undefined');
-    // }
-
-    if (!bearerHeader) {
-        return res.status(401).json({ Error: "Authentication failed. Token not provided." });
-    }
-
-    const token = bearerHeader.split(' ')[1];
-
-
-
-    //const token = bearerHeader.split(' ')[1];
-    console.log(token, "token")
-
-
-    if (!token) {
-        return res.json({ Error: "You are not authenticated" })
-    } else {
-        jwt.verify(token, secretKey, (err, decoded) => {
-            if (err) {
-                return res.json({ Error: "Token is not correct" })
-            } else {
-                req.email = decoded.email,
-                    req.password = decoded.password;
-                next();
-            }
-        })
-    }
-}
-
-export default verifyUser
-
 // import jwt from 'jsonwebtoken';
 
 // const secretKey = "8325731cde8293dbe25e329450ae2aa60486002f6eba23faacb69a9a18952e43";
 
 // const verifyUser = (req, res, next) => {
-//     console.log('in verifyUser')
-//     console.log("req.headers", req.headers);
 
-//     // Try to extract the token from the 'Authorization' header
+//     console.log("req.headers" , req.headers);
 //     const bearerHeader = req.headers['authorization'];
-//     let token;
+//     console.log("bearerHeader" , bearerHeader, )
 
-//     if (bearerHeader) {
-//         token = bearerHeader.split(' ')[1];
-//     } else {
-//         // If 'Authorization' header is not present, try to extract the token from the 'Cookie' header
-//         const cookieHeader = req.headers['cookie'];
-//         console.log('cookieHeader' , cookieHeader)
-//         if (cookieHeader) {
-//             const tokenMatch = cookieHeader.match(/token=([^;]+)/);
-//             token = tokenMatch && tokenMatch[1];
-//         }
+//     // if (bearerHeader) {
+//     //     const token = bearerHeader.split(' ')[1];
+//     //     // Rest of your code using 'token'
+//     // } else {
+//     //     // Handle the case when 'bearerHeader' is undefined
+//     //     console.error('Bearer header is undefined');
+//     // }
+
+//     if (!bearerHeader) {
+//         return res.status(401).json({ Error: "Authentication failed. Token not provided." });
 //     }
 
-//     console.log("token", token);
+//     const token = bearerHeader.split(' ')[1];
+
+
+
+//     //const token = bearerHeader.split(' ')[1];
+//     console.log(token, "token")
+
 
 //     if (!token) {
-//         return res.status(401).json({ Error: "Authentication failed. Token not provided." });
+//         return res.json({ Error: "You are not authenticated" })
 //     } else {
 //         jwt.verify(token, secretKey, (err, decoded) => {
 //             if (err) {
-//                 return res.status(401).json({ Error: "Token is not correct" });
+//                 return res.json({ Error: "Token is not correct" })
 //             } else {
-//                 req.email = decoded.email;
-//                 req.password = decoded.password;
+//                 req.email = decoded.email,
+//                     req.password = decoded.password;
 //                 next();
 //             }
-//         });
+//         })
 //     }
 // }
 
-// export default verifyUser;
+// export default verifyUser
+
+import jwt from 'jsonwebtoken';
+
+const secretKey = "8325731cde8293dbe25e329450ae2aa60486002f6eba23faacb69a9a18952e43";
+
+const verifyUser = (req, res, next) => {
+    console.log('in verifyUser')
+    console.log("req.headers", req.headers);
+
+    // Try to extract the token from the 'Authorization' header
+    const bearerHeader = req.headers['authorization'];
+    let token;
+
+    if (bearerHeader) {
+        token = bearerHeader.split(' ')[1];
+    } else {
+        // If 'Authorization' header is not present, try to extract the token from the 'Cookie' header
+        const cookieHeader = req.headers['cookie'];
+        console.log('cookieHeader' , cookieHeader)
+        if (cookieHeader) {
+            const tokenMatch = cookieHeader.match(/token=([^;]+)/);
+            token = tokenMatch && tokenMatch[1];
+        }
+    }
+
+    console.log("token", token);
+
+    if (!token) {
+        return res.status(401).json({ Error: "Authentication failed. Token not provided." });
+    } else {
+        jwt.verify(token, secretKey, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ Error: "Token is not correct" });
+            } else {
+                req.email = decoded.email;
+                req.password = decoded.password;
+                next();
+            }
+        });
+    }
+}
+
+export default verifyUser;
